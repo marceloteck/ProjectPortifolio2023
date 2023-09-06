f8e695b62f4ce2005fec78b3d677d925a0218756
Marcelo henrique <marcellosh.12@gmail.com>
2023-09-06 14:36:55 -0300

    Update curriculoEmail.vue

--- resources/PagesVuejs/Pages/index/curriculoEmail.vue
+++ resources/PagesVuejs/Pages/index/curriculoEmail.vue
@@ -11,14 +11,14 @@
                     <input type="email" :style="inputBtnStyles" v-model="emailUser" class="inputBtn" placeholder="Digite seu E-mail">
                     <button type="button" class="btn button shadow-lg bg-body rounded">
                         <div v-if="emailUser !== ''">
-                            <object class="svgiconUp" data="Assets/image/svg/send.svg" type="image/svg+xml"></object>
+                            <div class="svgiconUp" v-html="svgIconBtn.iconSend"></div>
                             <span>Enviar Email</span>
                         </div>
                         <div v-else>Passe o Mouse</div>
                     </button>
                     <button type="button" class="btn text-light DownBtn">
                         <div class="Btn">
-                            <object class="svgIcon" data="Assets/image/svg/downloadCv.svg" type="image/svg+xml"></object>
+                            <div class="svgIcon" v-html="svgIconBtn.iconDown"></div>
                             <span class="icon2"></span>
                             <span class="tooltip">Download</span>
                         </div>
@@ -41,7 +41,10 @@
             width: emailUser.value !== "" ? "255px" : "155px",
         };
     });
-
+const svgIconBtn = {
+    iconSend: '<svg fill="currentColor"  height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z" fill="currentColor"></path></svg>',
+    iconDown: '<svg fill="currentColor"  viewBox="0 0 384 512" height="1em" xmlns="http://www.w3.org/2000/svg"> <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"></path></svg>',
+}
 </script>
 
 <style lang="scss" scoped>
@@ -136,15 +139,47 @@ $corTexto: #ffffff;
                     position: relative;
                     transition-duration: .3s;
                     box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.11);
-                }
 
-                .svgIcon svg {
-                    fill: rgb(246, 255, 117);
-                    color: rgb(246, 255, 117);
+                    .svgIcon {
+                        fill: #f1444d;
+                    }
+                    .icon2 {
+                        width: 18px;
+                        height: 5px;
+                        border-bottom: 2px solid #f1444d;
+                        border-left: 2px solid #f1444d;
+                        border-right: 2px solid #f1444d;
+                    }
+                    .tooltip {
+                        position: absolute;
+                        right: -105px;
+                        opacity: 0;
+                        background: rgb(255, 255, 255);
+                        color: rgb(0, 0, 0);
+                        padding: 5px 10px;
+                        border-radius: 5px;
+                        border: 1px solid rgb(255, 255, 255);
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        transition-duration: .2s;
+                        pointer-events: none;
+                        letter-spacing: 0.5px;
+                    }
+                    .tooltip::before {
+                        position: absolute;
+                        content: "";
+                        width: 10px;
+                        height: 10px;
+                        background-color: #ffffff;
+                        background-size: 1000%;
+                        background-position: center;
+                        transform: rotate(45deg);
+                        left: -5%;
+                        transition-duration: .3s;
+                    }
                 }
             }
-            
-
         }
         .formBtn:hover{
             .inputBtn {
@@ -155,42 +190,6 @@ $corTexto: #ffffff;
             .DownBtn {
                 opacity: 1 !important;
                 margin-left: -5px !important;
-
-                .icon2 {
-                    width: 18px;
-                    height: 5px;
-                    border-bottom: 2px solid rgb(246, 255, 117);
-                    border-left: 2px solid rgb(246, 255, 117);
-                    border-right: 2px solid rgb(246, 255, 117);
-                }
-                .tooltip {
-                    position: absolute;
-                    right: -105px;
-                    opacity: 0;
-                    background: rgb(255, 255, 255);
-                    color: rgb(0, 0, 0);
-                    padding: 5px 10px;
-                    border-radius: 5px;
-                    border: 1px solid rgb(255, 255, 255);
-                    display: flex;
-                    align-items: center;
-                    justify-content: center;
-                    transition-duration: .2s;
-                    pointer-events: none;
-                    letter-spacing: 0.5px;
-                }
-                .tooltip::before {
-                    position: absolute;
-                    content: "";
-                    width: 10px;
-                    height: 10px;
-                    background-color: #ffffff;
-                    background-size: 1000%;
-                    background-position: center;
-                    transform: rotate(45deg);
-                    left: -5%;
-                    transition-duration: .3s;
-                }
             }
             .DownBtn:hover {
                 transition-duration: .3s;
