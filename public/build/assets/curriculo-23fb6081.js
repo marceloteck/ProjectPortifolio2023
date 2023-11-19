d224800d1df178c52c2f30ca516c3a573c613c76
Marcelo henrique <120139095+marceloteck@users.noreply.github.com>
2023-11-19 11:52:38 -0300

    update productions

--- public/build/assets/curriculo-23fb6081.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,r as e,o as a,c as r,a as c}from"./app-4d417d15.js";const s={};function p(l,u){const o=e("AppHead"),n=e("navCv"),_=e("mainCv");return a(),r("div",null,[c(o,{title:"Curriculo"}),c(n),c(_)])}const i=t(s,[["render",p],["__scopeId","data-v-5fc53a09"]]);export{i as default};
